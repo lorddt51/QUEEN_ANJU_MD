ecbeb742f601a5ce070a5ef4f499ff8926f04c6f
lorddt51 <142929809+lorddt51@users.noreply.github.com>
2024-11-15 09:39:13 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=SMNV1Zoa#vp1SG8XyZy2m7CriN8K45F0dXYm5Hi5dWNKIVMpQRLc",
 };
